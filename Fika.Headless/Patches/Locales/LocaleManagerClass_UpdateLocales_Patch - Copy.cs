93f1acea12a6306451574396011a6dbae8202002
Lacyway <20912169+Lacyway@users.noreply.github.com>
2025-06-09 15:57:41 +0200

    Add locale patch

---
+++ Fika.Headless/Patches/Locales/LocaleManagerClass_UpdateLocales_Patch - Copy.cs
@@ -0,0 +1,20 @@
+﻿using Fika.Core.Patching;
+using System.Reflection;
+
+namespace Fika.Headless.Patches.Locales
+{
+    public class LocaleManagerClass_method_4_Patch : FikaPatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return typeof(LocaleManagerClass).GetMethod(nameof(LocaleManagerClass.method_4));
+        }
+
+        [PatchPrefix]
+        public static bool Prefix(ref string __result)
+        {
+            __result = string.Empty;
+            return false;
+        }
+    }
+}
