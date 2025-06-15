334d4359b1612dfc6ecc370c928d1dc9705621e2
Lacyway <20912169+Lacyway@users.noreply.github.com>
2025-06-16 00:06:08 +0200

    Add patch

---
+++ Fika.Headless/Patches/Audio/BaseSpatialAudioPortal_Awake_Transpiler - Copy.cs
@@ -0,0 +1,22 @@
+﻿using Fika.Core.Patching;
+using HarmonyLib;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace Fika.Headless.Patches.Audio
+{
+    internal class AudioSourceCulling_method_1_Transpiler : FikaPatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return typeof(AudioSourceCulling).GetMethod(nameof(AudioSourceCulling.method_1));
+        }
+
+        [PatchTranspiler]
+        public static IEnumerable<CodeInstruction> Transpile()
+        {
+            yield return new(OpCodes.Ret);
+        }
+    }
+}
