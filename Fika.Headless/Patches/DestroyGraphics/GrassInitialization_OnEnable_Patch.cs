84f725271a5ccb982a58184d9515aebd0d31d1ff
Lacyway <20912169+Lacyway@users.noreply.github.com>
2025-10-15 08:44:05 +0200

    More nullref fixes

---
+++ Fika.Headless/Patches/DestroyGraphics/GrassInitialization_OnEnable_Patch.cs
@@ -0,0 +1,22 @@
+﻿using SPT.Reflection.Patching;
+using System.Reflection;
+
+namespace Fika.Headless.Patches.DestroyGraphics;
+
+public class LaserBeam_Awake_Patch : ModulePatch
+{
+    protected override MethodBase GetTargetMethod()
+    {
+        return typeof(LaserBeam)
+            .GetMethod(nameof(LaserBeam.Awake));
+    }
+
+    [PatchPrefix]
+    public static bool Prefix(LaserBeam __instance, ref Mesh ___mesh_0, ref Mesh ___mesh_1)
+    {
+        ___mesh_0 = new();
+        ___mesh_1 = new();
+        Object.Destroy(__instance);
+        return false;
+    }
+}
