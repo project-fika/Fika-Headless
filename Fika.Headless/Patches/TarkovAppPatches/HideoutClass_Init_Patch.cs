02ec9bc60ada36133230a7cf0b5e81286115d137
Lacyway <20912169+Lacyway@users.noreply.github.com>
2025-10-31 09:57:07 +0100

    Don't load hideout on headless

---
+++ Fika.Headless/Patches/TarkovAppPatches/HideoutClass_Init_Patch.cs
@@ -0,0 +1,24 @@
+﻿using SPT.Reflection.Patching;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Fika.Headless.Patches.TarkovAppPatches;
+
+/// <summary>
+/// Stops the headless from loading the hideout
+/// </summary>
+public class HideoutClass_Init_Patch : ModulePatch
+{
+    protected override MethodBase GetTargetMethod()
+    {
+        return typeof(HideoutClass)
+            .GetMethod(nameof(HideoutClass.Init));
+    }
+
+    [PatchPrefix]
+    public static bool Prefix(ref Task __result)
+    {
+        __result = Task.CompletedTask;
+        return false;
+    }
+}
