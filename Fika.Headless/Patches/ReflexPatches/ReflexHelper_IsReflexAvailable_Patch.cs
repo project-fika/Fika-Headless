a9df57ec288390fea0c4eb83012938ab2364c821
Lacyway <20912169+Lacyway@users.noreply.github.com>
2025-10-31 10:47:32 +0100

    Add more reflex patches

---
+++ Fika.Headless/Patches/ReflexPatches/ReflexHelper_IsReflexAvailable_Patch.cs
@@ -0,0 +1,24 @@
+﻿using NVIDIA;
+using SPT.Reflection.Patching;
+using System.Linq;
+using System.Reflection;
+
+namespace Fika.Headless.Patches.ReflexPatches;
+
+public class ReflexHelper_IsReflexAvailable_Patch : ModulePatch
+{
+    protected override MethodBase GetTargetMethod()
+    {
+        return typeof(ReflexHelper).
+            GetMethods().
+            FirstOrDefault(x => x.GetParameters().Length == 1);
+    }
+
+    [PatchPrefix]
+    public static bool Prefix(ref bool __result, out Reflex.NvReflex_Status reflexStatus)
+    {
+        reflexStatus = Reflex.NvReflex_Status.NvReflex_ERROR;
+        __result = false;
+        return __result;
+    }
+}
