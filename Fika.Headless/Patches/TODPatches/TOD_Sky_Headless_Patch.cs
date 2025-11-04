11a10943531162d5510d4a98c505a6dccf771711
Lacyway <20912169+Lacyway@users.noreply.github.com>
2025-11-04 18:53:38 +0100

    Add new patch and update refs

---
+++ Fika.Headless/Patches/TODPatches/TOD_Sky_Headless_Patch.cs
@@ -0,0 +1,21 @@
+﻿using SPT.Reflection.Patching;
+using System.Reflection;
+
+namespace Fika.Headless.Patches.TODPatches;
+
+internal class TOD_Sky_Headless_Patch : ModulePatch
+{
+    protected override MethodBase GetTargetMethod()
+    {
+        return typeof(TOD_Sky)
+            .GetProperty(nameof(TOD_Sky.Headless))
+            .GetGetMethod();
+    }
+
+    [PatchPrefix]
+    public static bool Prefix(ref bool __result)
+    {
+        __result = true;
+        return false;
+    }
+}
