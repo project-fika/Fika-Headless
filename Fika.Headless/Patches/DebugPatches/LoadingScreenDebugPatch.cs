4a8c00989bb4352c4806368b96ad47c4b88d22c2
Lacyway <20912169+Lacyway@users.noreply.github.com>
2025-11-26 09:28:02 +0100

    Change formatting

--- Fika.Headless/Patches/DebugPatches/LoadingScreenDebugPatch.cs
+++ Fika.Headless/Patches/DebugPatches/LoadingScreenDebugPatch.cs
@@ -4,7 +4,6 @@ using System.Reflection;
 
 namespace Fika.Headless.Patches.DebugPatches;
 
-[DebugPatch]
 internal class LoadingScreenDebugPatch : ModulePatch
 {
     private static int _lastProgress = -1;
@@ -17,16 +16,23 @@ internal class LoadingScreenDebugPatch : ModulePatch
     [PatchPrefix]
     public static bool Prefix(float pr)
     {
-        if (!FikaHeadlessPlugin.ShowDebugLogging.Value) return false;
+        if (!FikaHeadlessPlugin.ShowDebugLogging.Value)
+        {
+            return false;
+        }
+
         pr *= 100;
-        if ((int)pr == _lastProgress) return false;
+        if ((int)pr == _lastProgress)
+        {
+            return false;
+        }
+
         _lastProgress = (int)pr;
         Logger.LogInfo($"Loading Map: {(int)pr}%");
         return false;
     }
 }
 
-[DebugPatch]
 internal class LoadingScreenDebugPatch2 : ModulePatch
 {
     private static int _lastProgress = -1;
@@ -39,16 +45,23 @@ internal class LoadingScreenDebugPatch2 : ModulePatch
     [PatchPrefix]
     public static bool Prefix(float totalProgress)
     {
-        if (!FikaHeadlessPlugin.ShowDebugLogging.Value) return false;
+        if (!FikaHeadlessPlugin.ShowDebugLogging.Value)
+        {
+            return false;
+        }
+
         totalProgress *= 100;
-        if ((int)totalProgress == _lastProgress) return false;
+        if ((int)totalProgress == _lastProgress)
+        {
+            return false;
+        }
+
         _lastProgress = (int)totalProgress;
         Logger.LogInfo($"Caching Data: {totalProgress}%");
         return false;
     }
 }
 
-[DebugPatch]
 internal class LoadingScreenDebugPatch3 : ModulePatch
 {
     private static int _lastProgress = -1;
@@ -61,16 +74,23 @@ internal class LoadingScreenDebugPatch3 : ModulePatch
     [PatchPrefix]
     public static bool Prefix(float pr)
     {
-        if (!FikaHeadlessPlugin.ShowDebugLogging.Value) return false;
+        if (!FikaHeadlessPlugin.ShowDebugLogging.Value)
+        {
+            return false;
+        }
+
         pr *= 100;
-        if ((int)pr == _lastProgress) return false;
+        if ((int)pr == _lastProgress)
+        {
+            return false;
+        }
+
         _lastProgress = (int)pr;
         Logger.LogInfo($"Loading Auto-Culling: {pr}%");
         return false;
     }
 }
 
-[DebugPatch]
 internal class LoadingScreenDebugPatch4 : ModulePatch
 {
     private static int _lastProgress = -1;
@@ -83,9 +103,17 @@ internal class LoadingScreenDebugPatch4 : ModulePatch
     [PatchPrefix]
     public static bool Prefix(float pr)
     {
-        if (!FikaHeadlessPlugin.ShowDebugLogging.Value) return false;
+        if (!FikaHeadlessPlugin.ShowDebugLogging.Value)
+        {
+            return false;
+        }
+
         pr *= 100;
-        if ((int)pr == _lastProgress) return false;
+        if ((int)pr == _lastProgress)
+        {
+            return false;
+        }
+
         _lastProgress = (int)pr;
         Logger.LogInfo($"Loading Spatial Audio: {pr}%");
         return false;
