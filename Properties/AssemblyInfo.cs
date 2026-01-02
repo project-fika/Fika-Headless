d23e76f713edbc9249066afb46e4fa010e9133be
Lacyway <20912169+Lacyway@users.noreply.github.com>
2026-01-02 16:25:35 +0100

    Update copyright year

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyDescription("Play with friends in SPT - Headless")]
 [assembly: AssemblyCompany("https://github.com/project-fika")]
-[assembly: AssemblyCopyright("Copyright © Lacyway 2024")]
+[assembly: AssemblyCopyright("Copyright © Lacyway 2026")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion(FikaHeadlessPlugin.HeadlessVersion)]
