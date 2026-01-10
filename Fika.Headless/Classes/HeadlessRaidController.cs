410c63e8bc9030e2ee84d59b2e0c48ab6597cd4b
Lacyway <20912169+Lacyway@users.noreply.github.com>
2026-01-10 20:12:22 +0100

    Change transform update rate

--- Fika.Headless/Classes/HeadlessRaidController.cs
+++ Fika.Headless/Classes/HeadlessRaidController.cs
@@ -1,20 +1,12 @@
 ﻿namespace Fika.Headless.Classes;
 
 /// <summary>
-/// Used to sync transforms every 1 seconds, as otherwise triggers don't work for e.g. <see cref="EFT.Interactive.TransitPoint"/>s
+/// Used to sync transforms every frame, as otherwise triggers don't work for e.g. <see cref="EFT.Interactive.TransitPoint"/>s
 /// </summary>
 public class HeadlessRaidController : MonoBehaviour
 {
-    private readonly float _updateFreq = 1f;
-    private float _counter = 0f;
-
     protected void Update()
     {
-        _counter += Time.unscaledDeltaTime;
-        if (_counter >= _updateFreq)
-        {
-            _counter -= _updateFreq;
-            Physics.SyncTransforms();
-        }
+        Physics.SyncTransforms();
     }
 }
